8a2351701d0175d902f55d1e0c2dfe2110b9505c
SinthiyaAlex <sinthiyarosalin@thestackly.com>
2026-05-12 20:34:01 +0530

    task2

---
+++ task2.js
@@ -0,0 +1,155 @@
+// Task 1
+ console.log("10" + 5)
+//ans: string 105
+console.log(10 + true)
+//ans: number 11
+console.log(false + null)
+//ans: number 0
+console.log("Hello" + undefined)
+//ans: sring helloundefined
+console.log([1,2] + 5)
+//ans : string 1,25
+
+//Task 2
+console.log(typeof("hai" + 4));
+//ans: String
+console.log(typeof(true + 2));
+//ans: number
+console.log(typeof([1,2] + "hai"));
+//ans: string
+console.log(typeof({a:1} + 6));
+//ans: string
+console.log(typeof(null + 7));
+//ans: number
+
+//Task 3
+console.log(Number("500"));   
+// ans: 500
+console.log(Number(true));    
+// ans :1
+console.log(Number(false));   
+// ans: 0
+console.log(Number(null));   
+// ans: 0
+console.log(Number("abc"));   
+// ans: NaN 
+console.log(Number([100]));   
+// ans:  100
+
+//Task 4:
+console.log(Boolean(""));         
+//ans: false
+console.log(Boolean("javascript")); 
+//ans:  true
+console.log(Boolean(0));           
+//ans:  false
+console.log(Boolean(1));          
+ //ans:  true
+console.log(Boolean(null));       
+ //ans:  false
+console.log(Boolean(undefined));   
+//ans: false
+console.log(Boolean([]));          
+//ans: true
+console.log(Boolean({}));          
+//ans:  true
+
+
+//Task 5:
+let mark=45
+if (mark>35) {
+  console.log("Pass");
+}
+else{
+  console.log("Fail");
+  
+}
+//ans :Pass
+
+//Task 6:
+let age=20
+if (age>=18) {
+  console.log("Eligible");
+}
+else{
+  console.log("Not Eligible");
+  
+}
+//ans: Eligible
+
+//Task 7:
+let a = 50
+let b = 80
+let c = 30
+if (a > b && a > c) {
+    console.log("a is greatest:", a);
+} else if (b > a && b > c) {
+    console.log("b is greatest:", b);
+} else {
+    console.log("c is greatest:", c);
+}
+//ans: b is greatest 80
+
+//Task 8:
+let signal = "red";
+
+switch (signal) {
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Ready");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Signal is not working");
+}
+//ans: stop
+
+//Task 9:
+let username = "admin";
+let password = "1234";
+
+if (username === "Mary") {
+    if (password === "5896") {
+        console.log("Login Success");
+    } else {
+        console.log("Login Failed");
+    }
+} else {
+    console.log("Invalid Login");
+}
+//ans: Invalid Login
+
+//Task:10
+let hour = 14
+
+if (hour >= 1 && hour <= 12) {
+    console.log("Morning");
+} else if (hour >= 13 && hour <= 15) {
+  console.log("Afternoon");
+} else if (hour >= 16 && hour <= 19) {
+    console.log("Evening");
+} else if (hour >= 20 && hour <= 24) {
+  console.log("Night");
+} else {
+    console.log("Invalid hour");
+}
+// ans: afternoon
+
+//console.log(true + true)
+//ans :2
+//console.log("5" - 2)
+//ans :3
+//console.log("5" + 2)
+//ans:7
+//console.log(null + 1)
+//ans: null
+//console.log(undefined + 1)
+//ans: nan
+//console.log(Boolean(" "))
+//ans: srting with space is considered true
+//console.log(Number(true))
+//ans:1
