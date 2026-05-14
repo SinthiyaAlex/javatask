aa637aa9d88da8bcdb9892b4d9e06717ce8fab96
SinthiyaAlex <sinthiyarosalin@thestackly.com>
2026-05-14 17:18:24 +0530

    task5.js

---
+++ taask5.js
@@ -0,0 +1,145 @@
+ let studentname = prompt("Enter your Name :");
+ const department = prompt("Enter Your Department :");
+ let age = prompt("Enter your Age :");
+ console.log(`Welcome ${studentname}`);
+ console.log(`Department ${department}`);
+ console.log(`Age ${age}`);
+//1 Ans : Welcome sinthiya
+//        Department cse
+//        Age 30
+
+ const balance = 10000;
+ let withdrawal = prompt("Enter the amount to Withdraw");
+ withdrawal = Number(withdrawal);
+ if(withdrawal >= 100 && withdrawal <= balance){
+     console.log("Transaction sucessful");
+     console.log(`Remaining balance : ${balance-withdrawal}`);  
+ }
+ else if(withdrawal < 100){
+     console.log("Minimum withdraw is 100");
+ }
+ else{
+     console.log("Insufficient balance");  
+ }
+
+//2 ans if i give 8000
+//    it shows transaction success
+//    Remaining balance 2000
+// if i give 60
+// it shows Minimum withdrawal is 100
+//if i give 25000
+//it shows Insufficient balance
+
+ let amount = prompt("Enter the amount");
+ let order = (amount > 499) ? "free Delivery Available" : "Delivery charges applicable";
+ console.log(order);
+
+// 3. if i enter 300
+// it shows Delivery charges applicable
+// if i enter 500
+// it shows Free Delivery Available
+
+ let username = prompt("Enter the user name");
+ let password = prompt("Enter the valid password");
+ if(username === "sinthiya")
+     {
+     if(password === "1906") {
+         console.log("login success");      
+     }else{
+         console.log("Wrong Password");
+         }
+     }else{
+             console.log("Invalid user");       
+         }
+
+// 4 ans: if i give sinthiya and 1906 it show login success
+//     if i give sinthiya and 1234 it shows wrong password
+//     if i give admin and  1906 it shows invalid user
+
+
+ let signal = prompt("Enter the Signal");
+ switch (signal) {
+     case "green":
+         console.log("You can go");            
+         break;
+         case "yellow":
+             console.log("You have to get ready");
+             break;
+             case "red":
+                 console.log("You have to stop");
+                 break;                     
+     default:
+         console.log("signal is not Working");
+         break;
+ }
+
+// 5 ans: in case if change the case also (for example green instead Green) it shows signal is not working
+
+ function salaryCalculation(salary,bonus) {
+     return salary + bonus;    
+ }
+ console.log(salaryCalculation(35000,5000));
+
+// 6 ans 40000
+
+let price = [200 , 400 , 600 , 800];
+let total = 0;
+for (let i=0; i < price.length; i++){
+    total = total + price[i];
+}
+let average = total / price.length;
+console.log(`Total Price : ${total}`);
+console.log(`Average :  ${average}`);
+
+//7 ans Total Price : 2000
+//      Average : 500
+
+let whatapp = {
+    name: "sinthiya",
+    phone :"9876543210",
+    status : "At Work",
+}
+for(let key in whatapp){
+    console.log(`${key} : ${whatapp[key]}`);
+    
+}
+//8 ans : name : sinthiya
+//        phone: 9876543210
+//        status : At Work
+
+function bookticket(moviename , callback) {
+    console.log(`Movie name for booking : ${moviename}`);
+    console.log("Ticket booked successfully !....");
+    callback();    
+}
+function payment(){
+    console.log("Processing Payment.........");
+    console.log("Transaction Successful");   
+    }
+    bookticket("Harry Potter" , payment);
+
+//9 ans: Movie name for booking : Harry Potter
+//       Ticket booked successfully !....
+//       Processing Payment.......
+//       Transaction Successful
+
+function* foodorder(){
+    yield "order confirmed";
+    yield "preparing food";
+    yield "out for delivery";
+    yield "order delivered";
+} 
+let order1 = foodorder();
+console.log(order1.next().value);
+console.log(order1.next().value);
+console.log(order1.next().value);
+console.log(order1.next().value);
+
+//10 ans  order confirmed
+//        preparing order
+//        out for delivery
+//        order delivered
+
+
+    
+
--- task4.js
+++ task4.js
@@ -1,168 +1,168 @@
-  console.clear();
-  function welcomeuser(name)
-  {
-     console.log("Welcome " + name);
- }
- welcomeuser("Naveen")
-//1. ans: Welcome Naveen
+//   console.clear();
+//   function welcomeuser(name)
+//   {
+//      console.log("Welcome " + name);
+//  }
+//  welcomeuser("Naveen")
+// //1. ans: Welcome Naveen
 
-  function squareNumber(num) {
-      return num * num;    
-  }
-  console.log(squareNumber(5));
- //2. ans:25
+//   function squareNumber(num) {
+//       return num * num;    
+//   }
+//   console.log(squareNumber(5));
+//  //2. ans:25
 
-  let employee ={
-      name : "Rahul",
-      salary : 50000
-  }
-  function employeeBonus(bonus){
-  employee.salary += bonus;
-  console.log(employee.name);
-  console.log(employee.salary);    
-  }
-  employeeBonus(5000);
- //3.ans:55000
- //   console.clear();
+//   let employee ={
+//       name : "Rahul",
+//       salary : 50000
+//   }
+//   function employeeBonus(bonus){
+//   employee.salary += bonus;
+//   console.log(employee.name);
+//   console.log(employee.salary);    
+//   }
+//   employeeBonus(5000);
+//  //3.ans:55000
+//  //   console.clear();
 
- function testing()
- {
-     var r =10;
-     let b = 20;
-     const c = 30;
-     console.log(r);
- }
- testing();
-
-
-//console.log(b);
-//console.log(c);
-//4.ans var can be accessible because it is a globe
-// other two reference error
+//  function testing()
+//  {
+//      var r =10;
+//      let b = 20;
+//      const c = 30;
+//      console.log(r);
+//  }
+//  testing();
 
 
-const add1 = (a ,b) => {
-    console.log(a + b);
+// //console.log(b);
+// //console.log(c);
+// //4.ans var can be accessible because it is a globe
+// // other two reference error
+
+
+// const add1 = (a ,b) => {
+//     console.log(a + b);
     
-}
-add1(6 , 5)
-//5.ans: 11
+// }
+// add1(6 , 5)
+// //5.ans: 11
 
-function multi(a ,b){
-    return a * b;
-}
-function calculate(callback , n1 , n2)
-{
-    return callback(n1 ,n2)
-}
-console.log(calculate(multi, 2, 8));
-//6. ans: 16
+// function multi(a ,b){
+//     return a * b;
+// }
+// function calculate(callback , n1 , n2)
+// {
+//     return callback(n1 ,n2)
+// }
+// console.log(calculate(multi, 2, 8));
+// //6. ans: 16
 
-function* offers() {
-    yield "50% OFF";
-    yield "Free Delivery";
-    yield "Cashback";
-}
-const gen = offers();
-console.log(gen.next().value);
-console.log(gen.next().value); 
-console.log(gen.next().value); 
+// function* offers() {
+//     yield "50% OFF";
+//     yield "Free Delivery";
+//     yield "Cashback";
+// }
+// const gen = offers();
+// console.log(gen.next().value);
+// console.log(gen.next().value); 
+// console.log(gen.next().value); 
 
-//7.ans: 50% OFF
-//       Free Delivery
-//       Cashback
+// //7.ans: 50% OFF
+// //       Free Delivery
+// //       Cashback
 
-function student1(name , course ="Javascript"){
-    console.log(name);
-    console.log(course);   
-}
-student1("Priya" , "MYSQL");
-student1("Jerrin")
+// function student1(name , course ="Javascript"){
+//     console.log(name);
+//     console.log(course);   
+// }
+// student1("Priya" , "MYSQL");
+// student1("Jerrin")
 
-//8.ans: Priya
-//       MYSQL
-//       Jerrin
-//       Javascript
+// //8.ans: Priya
+// //       MYSQL
+// //       Jerrin
+// //       Javascript
 
-function multiplication(a) {
-    return function(b){
-        return function(c){
-            return a*b*c;
-        };
-    };    
-}
-console.log(multiplication(2)(3)(4));
+// function multiplication(a) {
+//     return function(b){
+//         return function(c){
+//             return a*b*c;
+//         };
+//     };    
+// }
+// console.log(multiplication(2)(3)(4));
 
-//9. ans: 24
+// //9. ans: 24
 
-const arr1 = [1,2,3] ;
-const arr2 = [ 4,5,6];
-const merge = [...arr1, ...arr2];//spread operator
-console.log(merge);
+// const arr1 = [1,2,3] ;
+// const arr2 = [ 4,5,6];
+// const merge = [...arr1, ...arr2];//spread operator
+// console.log(merge);
 
-//10. ans: [1,2,3,4,5,6]
+// //10. ans: [1,2,3,4,5,6]
 
-const obj1 = { name: "Navi" };
-const obj2 = { role: "Developer" };
-const merged = { ...obj1, ...obj2 };
-console.log(merged);
+// const obj1 = { name: "Navi" };
+// const obj2 = { role: "Developer" };
+// const merged = { ...obj1, ...obj2 };
+// console.log(merged);
 
-//11.{name: 'navi', role: 'developer' }
+// //11.{name: 'navi', role: 'developer' }
 
-function numbers(...num) {
-  console.log(num);
-  let sum = 0;
-  for (let i = 0; i < num.length; i++) {
-    sum += num[i];
-  }
-  console.log(sum);
-}
-numbers(1, 2, 3, 4);
+// function numbers(...num) {
+//   console.log(num);
+//   let sum = 0;
+//   for (let i = 0; i < num.length; i++) {
+//     sum += num[i];
+//   }
+//   console.log(sum);
+// }
+// numbers(1, 2, 3, 4);
 
-//12 ans [1,2,3,4]
-//      10
+// //12 ans [1,2,3,4]
+// //      10
 
 
-let students = [];
-function addStudent(name, marks, callback) {
-  const student = { name, marks };
-  students = [...students, student]; // spread operator
-  callback(student); // callback after adding
-}
-function printStudents() {
-  console.log("All Students:", students);
-}
-function calculateMarks(...marks) { // rest operator
-  return marks.reduce((sum, val) => sum + val, 0);
-}
-function addBonus(studentName, bonus) {
-  students = students.map(s =>s.name === studentName ? { ...s, marks: s.marks + bonus } : s );
-  console.log(studentName, "got bonus:", bonus);
-}
-function menu(option, ...args) {
-  switch(option) {
-    case "add":
-      addStudent(args[0], args[1], (s) => console.log("Added:", s));
-      break;
-    case "print":
-      printStudents();
-      break;
-    case "total":
-      console.log("Total Marks:", calculateMarks(...args));
-      break;
-    case "bonus":
-      addBonus(args[0], args[1]);
-      break;
-    default:
-      console.log("Invalid option");
-  }
-}
-menu("add", "sinthiya", 80);
-menu("add", "jerrin", 70);
-menu("print");
-menu("total", 80, 70);
-menu("bonus", "sinthiya", 10);
-menu("print");
+// let students = [];
+// function addStudent(name, marks, callback) {
+//   const student = { name, marks };
+//   students = [...students, student]; // spread operator
+//   callback(student); // callback after adding
+// }
+// function printStudents() {
+//   console.log("All Students:", students);
+// }
+// function calculateMarks(...marks) { // rest operator
+//   return marks.reduce((sum, val) => sum + val, 0);
+// }
+// function addBonus(studentName, bonus) {
+//   students = students.map(s =>s.name === studentName ? { ...s, marks: s.marks + bonus } : s );
+//   console.log(studentName, "got bonus:", bonus);
+// }
+// function menu(option, ...args) {
+//   switch(option) {
+//     case "add":
+//       addStudent(args[0], args[1], (s) => console.log("Added:", s));
+//       break;
+//     case "print":
+//       printStudents();
+//       break;
+//     case "total":
+//       console.log("Total Marks:", calculateMarks(...args));
+//       break;
+//     case "bonus":
+//       addBonus(args[0], args[1]);
+//       break;
+//     default:
+//       console.log("Invalid option");
+//   }
+// }
+// menu("add", "sinthiya", 80);
+// menu("add", "jerrin", 70);
+// menu("print");
+// menu("total", 80, 70);
+// menu("bonus", "sinthiya", 10);
+// menu("print");
 
 
 
